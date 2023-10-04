af10e0b8ef7232e78478413e95fb9e658ef9ff93
pesant1 <tran.thai@dawsoncollege.qc.ca>
2023-10-04 13:10:16 -0400

    forms post and get

--- get-book.php
+++ get-book.php
@@ -7,17 +7,16 @@
 <body>
 
 <body style="background-color:gray;">
-
-<p> Hello, welcome to Lab 5 </p>
  
-<h1> Task 3.2.5 (User Agent) </h1>
-<p><?= $_SERVER['HTTP_USER_AGENT'] ?></p>
 
-<h1> Task 3.3 (Forms-post) </h1>
-<form action="get-book.php" method = "post">
+<h1> Task 3.3 (Forms-get) </h1>
+<form action="get-book.php" method = "get">
     <p> books name: <input type="text" name="book_name" /></p>   
     <p> books genre: <input type="text" genre="genre_type" /></p>
     <input type="submit">                    
 </form>
+
+<p><?= var_dump($_GET) ?></p> 
+<p><?= var_dump($_POST) ?></p>
 
 </body>

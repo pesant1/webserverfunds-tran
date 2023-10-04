afb61ad5d373f1196751d6ff1dacf2b071358bf2
pesant1 <tran.thai@dawsoncollege.qc.ca>
2023-10-04 13:04:38 -0400

    Update lab5-webservers.php

--- lab5-webservers.php
+++ lab5-webservers.php
@@ -13,12 +13,11 @@
 <h1> Task 3.2.5 (User Agent) </h1>
 <p><?= $_SERVER['HTTP_USER_AGENT'] ?></p>
 
-<form>
-    <br>
-    <label for="genre">Genre:</label>
-    <input type="text" restrict onblur="field()" id="genre" pattern="(fantasy|modern|mystery|sci-fi)">  
-    <input type="submit">
-    <p id="required" style="display:none;">This field is required</p>                     
+<h1> Task 3.3 (Forms) </h1>
+<form action="lab5-webservers.php" method = "post">
+    <p> books name: <input type="text" name="book_name" /></p>   
+    <p> books genre: <input type="text" genre="genre_type" /></p>
+    <input type="submit">                    
 </form>
 
 </body>

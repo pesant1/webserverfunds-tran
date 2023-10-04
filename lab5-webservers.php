1c174fdb8a9d5e091f9909953e1c3d14719335f5
pesant1 <tran.thai@dawsoncollege.qc.ca>
2023-10-04 13:16:16 -0400

    Update lab5-webservers.php

--- lab5-webservers.php
+++ lab5-webservers.php
@@ -13,8 +13,8 @@
 <h1> Task 3.2.5 (User Agent) </h1>
 <p><?= $_SERVER['HTTP_USER_AGENT'] ?></p>
 
-<h1> Task 3.3 (Forms-post) </h1>
-<form action="get-book.php" method = "post">
+<h1> Task 3.3 (Forms-post and get) </h1>
+<form action="get-book.php" method = "get">
     <p> books name: <input type="text" name="book_name" /></p>   
     <p> books genre: <input type="text" name="genre_type" /></p>
     <input type="submit">                    

fdfeaffd6aca160a8fdb4565d2909b6bc27d2be3
pesant1 <tran.thai@dawsoncollege.qc.ca>
2023-10-04 13:15:49 -0400

    Update get-book.php

--- get-book.php
+++ get-book.php
@@ -9,12 +9,7 @@
 <body style="background-color:gray;">
  
 
-<h1> Task 3.3 (Forms-get) </h1>
-<form action="get-book.php" method = "get">
-    <p> books name: <input type="text" name="book_name" /></p>   
-    <p> books genre: <input type="text" genre="genre_type" /></p>
-    <input type="submit">                    
-</form>
+<h1> Task 3.3 (content of array get and post) </h1>
 
 <p><?= var_dump($_GET) ?></p> 
 <p><?= var_dump($_POST) ?></p>

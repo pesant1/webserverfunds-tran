b761e5f2c2aa761fe733f817c2525d16cef7c56c
pesant1 <tran.thai@dawsoncollege.qc.ca>
2023-10-04 13:44:31 -0400

    Update get-book.php

--- get-book.php
+++ get-book.php
@@ -16,8 +16,8 @@
 
 <h1> Task 3.3.5 (User friendly way) </h1>
 
-<p> Hello <?=htmlspecialchars($_GET['peoplename']) ?></p>
-<p> The name of book entered was: <?=htmlspecialchars($_GET['book_name']) ?></p>
+<p> Hello <?= htmlspecialchars($_GET['peoplename']) ?></p>
+<p> The name of book entered was: <?= htmlspecialchars($_GET['book_name']) ?></p>
 <p> The genre of the book entered was: <?=htmlspecialchars($_GET['genretype']) ?></p>
 
 </body>

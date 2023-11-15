39da9a77dec51219665b01bf3f644fab0ff286a6
pesant1 <tran.thai@dawsoncollege.qc.ca>
2023-11-15 13:28:14 -0500

    Update lab11.php

--- lab11.php
+++ lab11.php
@@ -10,26 +10,38 @@
 
 <p> Hello, welcome to Lab 11 </p>
  
+<?php 
+    $state =  $_POST["state"];
 
+    if ($state == "toggle")
+    {
+        `gpio toggle 7`;
+    }
+    else if ($state == "1")
+    {
+        `gpio write 7 1`;
+    }
+    else if ($state == "0")
+    {
+        `gpio write 7 0`;
+    }
+?>
 
 
 <p> On </p>
 <form method="post">
-
     <input type="hidden" name="state" value="1"/> 
     <input type="submit" value="toggle"/> 
 </form>
 
 <p> Off </p>
 <form method = "post">
-
     <input type="hidden" name="state" value="0"/> 
     <input type="submit" value="toggle"/> 
 </form>
 
 <p> Toggle </p>
 <form method = "post">
-
     <input type="hidden" name="state" value="toggle"/> 
     <input type="submit" value="toggle"/> 
 </form>

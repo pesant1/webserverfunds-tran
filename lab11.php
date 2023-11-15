0430448422803ee6d875734df2a28f1774e4573b
pesant1 <tran.thai@dawsoncollege.qc.ca>
2023-11-15 13:25:49 -0500

    Update lab11.php

--- lab11.php
+++ lab11.php
@@ -10,23 +10,7 @@
 
 <p> Hello, welcome to Lab 11 </p>
  
-<?php 
-    $state =  $_POST("state");
 
-    if ($state == "toggle")
-    {
-        `gpio toggle 7`;
-    }
-    else if ($state == "1")
-    {
-        `gpio write 7 1`;
-    }
-    else if ($state == "0")
-    {
-        `gpio write 7 0`;
-    }
-
-?>
 
 
 <p> On </p>

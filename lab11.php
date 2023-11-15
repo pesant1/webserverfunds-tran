73b81199660a53d144aeda33e1725c00ee94e39d
pesant1 <tran.thai@dawsoncollege.qc.ca>
2023-11-15 13:07:34 -0500

    Create lab11.php

---
+++ lab11.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Lab11:GPIO - Tran</title>
+</head>
+
+<body>
+
+<body style="background-color:gray;">
+
+<p> Hello, welcome to Lab 11 </p>
+ 
+state = <?php $state ?>
+
+<p> On </p>
+<form method = "post">
+
+    <input type="submit" value="on"/> 
+</form>
+
+<p> Off </p>
+<form method = "post">
+
+    <input type="submit" value="off"/> 
+</form>
+
+<p> Toggle </p>
+<form method = "post">
+
+    <input type="submit" value="toggle"/> 
+</form>
+
+</body>

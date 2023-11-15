96103f27552d703a6d0afd458a5b5357a6a2ae48
pesant1 <tran.thai@dawsoncollege.qc.ca>
2023-11-15 15:01:38 -0500

    Update lab11readings.php

--- lab11readings.php
+++ lab11readings.php
@@ -4,13 +4,12 @@
     <title>Lab11 readings - Tran</title>
 </head>
 <body style="background-color:gray;">
-<p><?php 
+<?php 
     $raw = `./bme280`;   
     $deserialized = json_decode($raw, true); 
 
 echo "temperature = {$deserialized["temperature"]} <br>"; 
 echo "pressure =  {$deserialized["pressure"]} <br>";
 echo "altitude =  {$deserialized["altitude"]<br>"; 
-
-?></p> 
+?>
 </body>

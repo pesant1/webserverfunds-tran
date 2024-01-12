3b3903bcaaa685a1ed136094c461aa2fbb47fdbf
pesant1 <tran.thai@dawsoncollege.qc.ca>
2024-01-12 11:50:50 -0500

    Create query.php

---
+++ query.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lab7: PHP and SQL Response</title>
+    <h1> Task 3.3 (content of array get and post) </h1>
+
+</head>
+
+<body>
+
+<body style="background-color:gray;">
+ 
+
+
+
+<p><?= var_dump($_GET) ?></p> 
+
+
+
+
+</body>
+</html>

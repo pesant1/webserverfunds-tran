fdaf6eafa0dcac9577f55ae998caacb68484a601
pesant1 <tran.thai@dawsoncollege.qc.ca>
2024-01-12 12:16:29 -0500

    Update finals.php

--- finals.php
+++ finals.php
@@ -31,14 +31,10 @@
 
     <form action="query.php" method = "get">
         <p> Enter your search: <input type="text" id="search" name="search" /></p> 
-        <?php
-            foreach($result as $row) 
-                { 
-                    echo "<option value='{$row['search']}'>{$row['ip']}</option>\n";
-                }
-                    mysqli_close($conn);
-        ?>
-        </select>     
+    <?php
+        $sql = "INSERT INTO finals (search, ip) VALUES ('$id', '$_SERVER['REMOTE_ADDR']');";
+        mysqli_close($conn);
+    ?>        
         
         <input type="submit"/> 
     </form>

4d4e560940962ee1d3f1804d836d5a624117a4ca
pesant1 <tran.thai@dawsoncollege.qc.ca>
2024-01-12 11:38:38 -0500

    Update finals.php

--- finals.php
+++ finals.php
@@ -10,6 +10,9 @@
 
     <p><?= $_SERVER['REMOTE_ADDR'] ?></p>
 
-
+    <form action="query.php" method = "get">
+        <p> Enter your search: <input type="text" id="search" name="search" /></p> 
+        <input type="submit"/> 
+    </form>
     
 </body>

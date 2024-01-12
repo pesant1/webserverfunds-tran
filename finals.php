2a5d837f354d6784feb60b7525719d60f7a5c9d6
pesant1 <tran.thai@dawsoncollege.qc.ca>
2024-01-12 12:31:51 -0500

    dsadsa

--- finals.php
+++ finals.php
@@ -30,7 +30,7 @@
     <p><?= $_SERVER['REMOTE_ADDR'] ?></p>
 
     <form action="query.php" method = "get">
-    <p> Enter your search: <input type="text" id="search" name="search" /></p> 
+    <p> Enter your search: <input type="text" required id="search" name="search" /></p> 
     <input type="submit" value="please"/> 
     
 
